a40ad0922fc09e0c69030196ca3737b4880ab5e8
MikoloD <61506836+MikoloD@users.noreply.github.com>
2021-10-14 23:06:53 +0200

    Initial commit

---
+++ GraphRouting/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using GraphRouting.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RoadNetwork;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphRouting.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly GraphDTO _graphDTO;
+
+        public HomeController(ILogger<HomeController> logger,GraphDTO graphDTO)
+        {
+            _logger = logger;
+            _graphDTO = graphDTO;
+        }
+
+        public IActionResult Index()
+        {
+            Dijkstra dijkstra = new Dijkstra(_graphDTO);
+            //for debug purposes 0
+            int startingVertex = 0;
+            dijkstra.Run(_graphDTO.AdjacencyMatrix, startingVertex);
+            return View(dijkstra.DijkstraDTOs);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RoadNetwork/GraphDTO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace RoadNetwork
+{
+    public class GraphDTO
+    {
+        public int[,] AdjacencyMatrix;
+        public Dictionary<int, string> Vercites; 
+        public int[,] LoadAdjacencyMatrix(string path)
+        {
+            string input = File.ReadAllText(path);
+            int i = 0;
+            int size = File.ReadAllLines(path).Length;
+            int[,] result = new int[size, size];
+            foreach (var row in input.Split('\n'))
+            {
+                int j = 0;
+                foreach (var col in row.Trim().Split(' '))
+                {
+                    var elem = col.Trim();
+                    if (Int32.TryParse(elem, out int defaultInt))
+                    {
+                        result[i, j] = int.Parse(elem);
+                        j++;
+                    }
+                    else if (j == 0 && !String.IsNullOrEmpty(elem))
+                    {
+                        Vercites.Add(i, elem);
+                    }
+                }
+                i++;
+            }
+            return result;
+        }
+        public GraphDTO()
+        {
+            Vercites = new Dictionary<int, string>();
+            AdjacencyMatrix = LoadAdjacencyMatrix(@"G:\Projekty\RoadNetwork\Data\Distances.txt");
+        }
+    }
+}
